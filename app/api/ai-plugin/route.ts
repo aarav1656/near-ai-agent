5131791d2c1680c91e99dce5f3fdcf30757563ea
kamalbuilds <geniusamansingh@gmail.com>
2025-01-26 17:56:56 +0530

    parse the quotes

--- app/api/ai-plugin/route.ts
+++ app/api/ai-plugin/route.ts
@@ -761,7 +761,93 @@ export async function GET() {
                   },
                 },
             },
-
+            "/api/tools/uniswap": {
+                post: {
+                  tags: ["uniswap"],
+                  operationId: "swap",
+                  summary:
+                    "Quote a price and fee for the specified order parameters. Posts unsigned order to Uniswap and returns Signable payload",
+                  description:
+                    "Given a partial order compute the minimum fee and a price estimate for the order. Return a full order that can be used directly for signing, and with an included signature, passed directly to the order creation endpoint.",
+                  parameters: [
+                    { $ref: "#/components/parameters/chainId" },
+                    { $ref: "#/components/parameters/safeAddress" },
+                    {
+                      in: "query",
+                      name: "sellToken",
+                      required: true,
+                      schema: {
+                        type: "string",
+                      },
+                      description:
+                        "The ERC-20 token symbol or address to be sold, if provided with the symbol do not try to infer the address.",
+                    },
+                    {
+                      in: "query",
+                      name: "buyToken",
+                      required: true,
+                      schema: {
+                        type: "string",
+                      },
+                      description:
+                        "The ERC-20 token symbol or address to be bought, if provided with the symbol do not try to infer the address..",
+                    },
+                    {
+                      in: "query",
+                      name: "receiver",
+                      required: false,
+                      schema: {
+                        type: "string",
+                      },
+                      description:
+                        "The address to receive the proceeds of the trade, instead of the sender's address.",
+                    },
+                    {
+                      in: "query",
+                      name: "sellAmountBeforeFee",
+                      required: true,
+                      schema: {
+                        type: "string",
+                      },
+                      description:
+                        "The amount of tokens to sell before fees, represented as a decimal string in token units. Not Atoms.",
+                    },
+                  ],
+                  // requestBody: {
+                  //   description: "The order parameters to compute a quote for.",
+                  //   required: true,
+                  //   content: {
+                  //     "application/json": {
+                  //       schema: {
+                  //         $ref: "#/components/schemas/OrderQuoteRequest",
+                  //       },
+                  //     },
+                  //   },
+                  // },
+                  responses: {
+                    "200": { $ref: "#/components/responses/SignRequestResponse200" },
+                    "400": {
+                      description: "Error quoting order.",
+                      content: {
+                        "application/json": {
+                          schema: {
+                            $ref: "#/components/schemas/PriceEstimationError",
+                          },
+                        },
+                      },
+                    },
+                    "404": {
+                      description: "No route was found for the specified order.",
+                    },
+                    "429": {
+                      description: "Too many order quotes.",
+                    },
+                    "500": {
+                      description: "Unexpected error quoting an order.",
+                    },
+                  },
+                },
+            },
         },
     };
 
